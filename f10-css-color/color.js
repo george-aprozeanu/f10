841f2b7589a9e757969ecaaa918652702df1cab3
Jorge <george.aprozeanu@teads.tv>
2018-04-26 11:02:12 +0300

    v1.2.3

---
+++ f10-css-color/color.js
@@ -0,0 +1,5 @@
+const chroma = require('chroma-js');
+
+console.log(chroma.lab(54, 0, 0
+    -0.000009997846384113274,
+    0.00000399913857584977 ).hex());
--- f10-stream/index.js
+++ f10-stream/index.js
@@ -1,1 +1,4 @@
-export * from './dist';
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./dist/index"));
